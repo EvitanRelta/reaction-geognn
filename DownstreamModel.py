d6c9a3c319c6826e4bc28d455824de99fe7e8584
EvitanRelta <shauntanzongzhi@gmail.com>
2023-06-06 17:03:10 +0800

    Create 'DownstreamModel' & 'DropoutMLP'

---
+++ DownstreamModel.py
@@ -0,0 +1,71 @@
+from typing import Literal
+from torch import Tensor, nn
+from GeoGNN import GeoGNNModel
+from DropoutMLP import DropoutMLP
+from dgl import DGLGraph
+
+
+class DownstreamModel(nn.Module):
+    """
+    Model that uses the graph-representation output from 
+    `self.compound_encoder: GeoGNNModel` to make `out_size` number of predictions.
+
+    This is a PyTorch + DGL equivalent of GeoGNN's `DownstreamModel`:
+    https://github.com/PaddlePaddle/PaddleHelix/blob/e93c3e9/apps/pretrained_compound/ChemRL/GEM/src/model.py#L27-L61
+    """
+    def __init__(
+        self,
+        compound_encoder: GeoGNNModel,
+        task_type: Literal['classification', 'regression'],
+        out_size: int,
+        num_of_layers: int = 2,
+        hidden_size: int = 128,
+        activation: nn.Module = nn.LeakyReLU(),
+        dropout_rate: float = 0.2
+    ):
+        """
+        Default values for `num_of_layers`, `hidden_size`, `activation` and
+        `dropout_rate` are based on GeoGNN's `down_mlp2.json` config:
+        https://github.com/PaddlePaddle/PaddleHelix/blob/e93c3e9/apps/pretrained_compound/ChemRL/GEM/model_configs/down_mlp2.json
+
+        where `down_mlp2.json` (not `down_mlp3.json`) is used by the GeoGNN's 
+        finetuning scripts:
+        https://github.com/PaddlePaddle/PaddleHelix/blob/e93c3e9/apps/pretrained_compound/ChemRL/GEM/scripts/finetune_class.sh
+        https://github.com/PaddlePaddle/PaddleHelix/blob/e93c3e9/apps/pretrained_compound/ChemRL/GEM/scripts/finetune_regr.sh
+        """
+        super(DownstreamModel, self).__init__()
+        self.task_type = task_type
+        self.out_size = out_size
+
+        self.compound_encoder = compound_encoder
+        self.norm = nn.LayerNorm(compound_encoder.embed_dim)
+        self.mlp = DropoutMLP(
+            num_of_layers = num_of_layers,
+            in_size = compound_encoder.embed_dim,
+            hidden_size = hidden_size,
+            out_size = out_size,
+            activation = activation,
+            dropout_rate = dropout_rate
+        )
+        if self.task_type == 'classification':
+            self.out_act = nn.Sigmoid()
+
+    def forward(self, atom_bond_graph: DGLGraph, bond_angle_graph: DGLGraph) -> Tensor:
+        """
+        Args:
+            atom_bond_graph (DGLGraph): Graph of a molecule, with atoms as \
+                nodes, bonds as edges.
+            bond_angle_graph (DGLGraph): Graph of a molecule, with bonds as \
+                nodes, bond-angles as edges.
+
+        Returns:
+            Tensor: Predicted values with size `(self.out_size, )`.
+        """
+        node_repr, edge_repr, graph_repr \
+            = self.compound_encoder.forward(atom_bond_graph, bond_angle_graph)
+        graph_repr = self.norm.forward(graph_repr)
+        pred = self.mlp.forward(graph_repr)
+
+        if self.task_type == 'classification':
+            return self.out_act.forward(pred)
+        return pred
---
+++ DropoutMLP.py
@@ -0,0 +1,53 @@
+from torch import nn, Tensor
+
+
+class DropoutMLP(nn.Module):
+    """
+    Multi-layer perceptron (MLP) with dropout layers.
+
+    This is a PyTorch equivalent of GeoGNN's `MLP`:
+    https://github.com/PaddlePaddle/PaddleHelix/blob/e93c3e9/pahelix/networks/basic_block.py#L42-L68
+    """
+    def __init__(
+        self,
+        num_of_layers: int,
+        in_size: int,
+        hidden_size: int,
+        out_size: int,
+        activation: nn.Module,
+        dropout_rate: float
+    ):
+        """
+        Args:
+            num_of_layers (int): Number of layers.
+            in_size (int): Input feature size.
+            hidden_size (int): Hidden layers' size.
+            out_size (int): Output feature size.
+            activation (nn.Module): Activation module (eg. `nn.ReLU()`).
+            dropout_rate (float): Dropout rate for the dropout layers.
+        """
+        super(DropoutMLP, self).__init__()
+        layers: list[nn.Module] = []
+        for layer_id in range(num_of_layers):
+            is_last_layer = layer_id == num_of_layers - 1
+            if is_last_layer:
+                layers.append(nn.Linear(hidden_size, out_size))
+                continue
+
+            is_first_layer = layer_id == 0
+            layers.extend([
+                nn.Linear(in_size if is_first_layer else hidden_size, hidden_size),
+                nn.Dropout(dropout_rate),
+                activation
+            ])
+        self.mlp = nn.Sequential(*layers)
+
+    def forward(self, x: Tensor) -> Tensor:
+        """
+        Args:
+            x (Tensor): Input features of size `(in_size, )`, as defined in the constructor.
+
+        Returns:
+            Tensor: Output features of size `(out_size, )`, as defined in the constructor.
+        """
+        return self.mlp.forward(x)
