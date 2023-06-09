1d7965a98eca1f5293d373a2d2686c31b655e9ba
EvitanRelta <shauntanzongzhi@gmail.com>
2023-06-09 09:41:26 +0800

    Reword/Rename the docs/args in 'SimpleGIN' to be more descriptive

--- SimpleGIN.py
+++ SimpleGIN.py
@@ -5,22 +5,34 @@ from typing import cast
 
 class SimpleGIN(nn.Module):
     """
-    Implementation of Graph Isomorphism Network (GIN) layer
-    for undirected graphs, that accounts for with edge features.
+    Implementation of Graph Isomorphism Network (GIN) layer for undirected
+    graphs that incorporates both node and edge features.
+    
+    This implementation does NOT transform the graph features before
+    message-passing, only AFTER the message-passing are the features passed
+    through a 2-layer MLP before being returned.
+
+    This is a PyTorch + DGL equivalent of GeoGNN's `GIN`:
+    https://github.com/PaddlePaddle/PaddleHelix/blob/e93c3e9/pahelix/networks/gnn_block.py#L75-L105
     """
-    def __init__(self, mlp_in_size: int, mlp_hidden_size: int, mlp_out_size: int) -> None:
+    def __init__(self, in_feat_size: int, mlp_hidden_size: int, out_feat_size: int) -> None:
         """
-        - `MLP` - Multilayer Perceptron
+        The "MLP" in this context is the final Multilayer Perceptron that is fed
+        the graph features after message-passing, where the MLP output is then
+        returned by the `forward` method.
+
         Args:
-            mlp_in_size (int): Input size of the MLP.
-            mlp_hidden_size (int): Hidden layer's size of the MLP.
-            mlp_out_size (int): Output size of the MLP.
+            in_feat_size (int): The size of each feature in the graph \
+                (if the feats were encoded into embeddings, this'll be the embedding size).
+            mlp_hidden_size (int): Hidden layer's size of the MLP \
+                (the MLP after message-passing).
+            out_feat_size (int): The output size for each feature.
         """
         super(SimpleGIN, self).__init__()
         self.mlp = nn.Sequential(
-            nn.Linear(mlp_in_size, mlp_hidden_size),
+            nn.Linear(in_feat_size, mlp_hidden_size),
             nn.ReLU(),
-            nn.Linear(mlp_hidden_size, mlp_out_size)
+            nn.Linear(mlp_hidden_size, out_feat_size)
         )
 
     def reset_parameters(self) -> None:
@@ -31,9 +43,17 @@ class SimpleGIN(nn.Module):
     def forward(self, graph: DGLGraph, node_feats: Tensor, edge_feats: Tensor) -> Tensor:
         """
         Args:
-            graph (DGLGraph): The input graph.
-            node_feat (Tensor): The input node features.
-            edge_feats (Tensor): The input edge features.
+            graph (DGLGraph): The input graph, where each node/edge feature is \
+                of size `in_feat_size`, as defined in the constructor.
+            node_feats (Tensor): The input node features, size `(num_of_nodes, in_feat_size)`, \
+                where `in_feat_size` is defined in the constructor.
+            edge_feats (Tensor): The input edge features, size `(num_of_edges, in_feat_size)`, \
+                where `in_feat_size` is defined in the constructor.
+
+        Returns:
+            Tensor: Output features that incorporates both node and edge features, \
+                size `(num_of_nodes, out_feat_size)`, where `out_feat_size` is \
+                defined in the constructor.
         """
         with graph.local_scope():
             graph.ndata['h_n'] = node_feats
