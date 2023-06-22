f38b9aa0e96c714fb06ef022c4a2672ca0509053
EvitanRelta <shauntanzongzhi@gmail.com>
2023-06-22 16:19:22 +0800

    Refactor 'reaction_geognn/preprocessing.py'

--- reaction_geognn/preprocessing.py
+++ reaction_geognn/preprocessing.py
@@ -12,32 +12,32 @@ from rdkit.Chem import AllChem
 from torch import Tensor
 
 
-def _validate_smiles(reactant_smiles: str, product_smiles: str) -> None:
-    # Prevents `AllChem.MolFromSmiles` from removing the atom-mapped hydrogens in the SMILES.
-    # Based on: https://github.com/rdkit/rdkit/discussions/4703#discussioncomment-1656372
-    smiles_parser = Chem.SmilesParserParams() # type: ignore
-    smiles_parser.removeHs = False
-
-    reactant_mol = AllChem.MolFromSmiles(reactant_smiles, smiles_parser) # type: ignore
-    product_mol = AllChem.MolFromSmiles(product_smiles, smiles_parser) # type: ignore
-
-    assert reactant_mol.GetNumAtoms() == product_mol.GetNumAtoms(), \
-        'Reaction SMART is not balanced.'
-
-    reactant_atom_map = [atom.GetAtomMapNum() for atom in reactant_mol.GetAtoms()]
-    product_atom_map = [atom.GetAtomMapNum() for atom in product_mol.GetAtoms()]
-    assert len(reactant_atom_map) == len(set(reactant_atom_map)), \
-        'Reactants have duplicate atom mapping numbers.'
-    assert len(product_atom_map) == len(set(product_atom_map)), \
-        'Products have duplicate atom mapping numbers.'
-    assert max(reactant_atom_map) == max(product_atom_map) == reactant_mol.GetNumAtoms(), \
-        "Atom mappings numbers don't match number of atoms."
-
-
 def reaction_smart_to_graph(
     reaction_smart: str,
     device: torch.device = torch.device('cpu')
 ) -> tuple[DGLGraph, DGLGraph]:
+    """
+    Converts a balanced and atom-mapped reaction's SMART string into 2 `DGLGraphs`:
+    - a graph with atoms as nodes, bonds as edges
+    - a graph with bonds as nodes, bond-angles as edges
+
+    The product's nodes/features are concatenated behind the reactant's
+    node/features in the graphs.
+
+    For the atom-bond graph, the nodes (ie. the atoms) are arranged according to
+    their atom-mapping number.
+    (ie. reactant atom with atom-mapping 1 is node 0 in the graph, since
+    atom-mappings are one-indexed while nodes are zero-indexed)
+
+    Args:
+        reaction_smart (str): A balanced and atom-mapped reaction's SMILES string.
+        device (torch.device, optional): The CPU/GPU to set returned graphs to use. \
+            Defaults to torch.device('cpu').
+
+    Returns:
+        tuple[DGLGraph, DGLGraph]: 1st graph is the atom-bond graph, 2nd \
+            is the bond-angle graph.
+    """
     # `reactant_smiles` contains all reactants, `product_smiles` contains all products.
     reactant_smiles, product_smiles = reaction_smart.split('>>')
     _validate_smiles(reactant_smiles, product_smiles)
@@ -62,7 +62,42 @@ def reaction_smart_to_graph(
     )
 
 
+def _validate_smiles(reactant_smiles: str, product_smiles: str) -> None:
+    """
+    Validate that the SMILES are:
+    - balanced (num of reactant atoms == num of product atoms).
+    - have valid atom-mappings.
+
+    Args:
+        reactant_smiles (str): SMILES string of all the reactants.
+        product_smiles (str): SMILES string of all the products.
+    """
+    # Prevents `AllChem.MolFromSmiles` from removing the atom-mapped hydrogens in the SMILES.
+    # Based on: https://github.com/rdkit/rdkit/discussions/4703#discussioncomment-1656372
+    smiles_parser = Chem.SmilesParserParams() # type: ignore
+    smiles_parser.removeHs = False
+
+    reactant_mol = AllChem.MolFromSmiles(reactant_smiles, smiles_parser) # type: ignore
+    product_mol = AllChem.MolFromSmiles(product_smiles, smiles_parser) # type: ignore
+
+    assert reactant_mol.GetNumAtoms() == product_mol.GetNumAtoms(), \
+        'Reaction SMART is not balanced.'
+
+    reactant_atom_map = [atom.GetAtomMapNum() for atom in reactant_mol.GetAtoms()]
+    product_atom_map = [atom.GetAtomMapNum() for atom in product_mol.GetAtoms()]
+    assert len(reactant_atom_map) == len(set(reactant_atom_map)), \
+        'Reactants have duplicate atom mapping numbers.'
+    assert len(product_atom_map) == len(set(product_atom_map)), \
+        'Products have duplicate atom mapping numbers.'
+    assert max(reactant_atom_map) == max(product_atom_map) == reactant_mol.GetNumAtoms(), \
+        "Atom mappings numbers don't match number of atoms."
+
+
 def _merge_graphs(graph_list: list[DGLGraph]) -> DGLGraph:
+    """
+    Merge multiple graphs into a single graph. Similar to `dgl.batch` but it
+    it doesn't "batch" the graphs.
+    """
     # Initialize empty lists for the new nodes and edges
     new_edges_src: list[int] = []
     new_edges_dst: list[int] = []
