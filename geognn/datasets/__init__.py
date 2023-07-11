d738cfd4d3bead3874c1f47123d00c5419ea0531
EvitanRelta <shauntanzongzhi@gmail.com>
2023-07-11 13:26:25 +0800

    Rename file 'GeoGNNDataLoader.py' -> 'data_loader.py'

--- geognn/datasets/__init__.py
+++ geognn/datasets/__init__.py
@@ -1,4 +1,4 @@
+from .data_loader import GeoGNNDataLoader_
 from .esol_dataset import ESOLDataset
-from .GeoGNNDataLoader import GeoGNNDataLoader_
 from .shared_definitions import GeoGNNDataElement, GeoGNNDataset
 from .splitters import ScaffoldSplitter
