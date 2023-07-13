4e5fb38139ea8d7b92cc539bafad7c483c93d9ea
EvitanRelta <shauntanzongzhi@gmail.com>
2023-07-13 12:06:58 +0800

    Replace scaffold-split with random-split in QM9 dataset

--- geognn/data_modules.py
+++ geognn/data_modules.py
@@ -1,13 +1,14 @@
+import math
 from abc import ABC
 
 import torch
 from dgl import DGLGraph
 from geognn_base_classes import GeoGNNDataElement
 from lightning_utils import GeoGNNCacheDataModule
-from torch.utils.data import Dataset
+from torch.utils.data import Dataset, random_split
 from typing_extensions import override
 
-from .datasets import QM9_TASK_COL_NAMES, QM9Dataset, ScaffoldSplitter
+from .datasets import QM9_TASK_COL_NAMES, QM9Dataset
 from .Preprocessing import Preprocessing
 
 
@@ -23,8 +24,23 @@ class QM9DataModule(_BaseDataModule):
     def get_dataset_splits(self) -> \
         tuple[Dataset[GeoGNNDataElement], Dataset[GeoGNNDataElement], Dataset[GeoGNNDataElement]]:
         dataset = QM9Dataset(self.task_column_name)
+
+        # Based on `random_split` from newer versions of Pytorch:
+        # https://pytorch.org/docs/stable/_modules/torch/utils/data/dataset.html#random_split
+        train_val_test_frac = [0.8, 0.1, 0.1]
+        subset_lengths: list[int] = []
+        for frac in train_val_test_frac:
+            n_items_in_split = int(math.floor(len(dataset) * frac))
+            subset_lengths.append(n_items_in_split)
+        remainder = len(dataset) - sum(subset_lengths)
+        # add 1 to all the lengths in round-robin fashion until the remainder is 0
+        for i in range(remainder):
+            idx_to_add_at = i % len(subset_lengths)
+            subset_lengths[idx_to_add_at] += 1
+
+        rand_gen = torch.Generator().manual_seed(self.random_split_seed)
         train_dataset, valid_dataset, test_dataset \
-            = ScaffoldSplitter().split(dataset, frac_train=0.8, frac_valid=0.1, frac_test=0.1)
+            = random_split(dataset, subset_lengths, generator=rand_gen)
         return train_dataset, test_dataset, valid_dataset
 
     def __init__(
@@ -33,6 +49,8 @@ class QM9DataModule(_BaseDataModule):
         batch_size: int,
         shuffle: bool = False,
         cache_path: str | None = None,
+        random_split_seed: int = 0,
     ):
         self.task_column_name = task_column_name
+        self.random_split_seed = random_split_seed
         super().__init__(batch_size, shuffle, cache_path)
