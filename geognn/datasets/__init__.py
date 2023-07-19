ec057d017ceec68f36de389209f38d58c9a3498f
EvitanRelta <shauntanzongzhi@gmail.com>
2023-07-19 13:56:54 +0800

    Remove legacy GeoGNN dataloader class

--- geognn/datasets/__init__.py
+++ geognn/datasets/__init__.py
@@ -1,4 +1,3 @@
-from .data_loader import GeoGNNDataLoader_
 from .esol_dataset import ESOLDataset
 from .qm9_dataset import QM9_TASK_COL_NAMES, QM9Dataset
 from .shared_definitions import load_smiles_csv
