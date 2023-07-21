c2c043058f862fad73469efe046bfdb5f7d94d59
EvitanRelta <shauntanzongzhi@gmail.com>
2023-07-21 14:06:58 +0800

    Update 'finetune_qm9_pretrained.sh' notes & path

--- finetune_qm9_pretrained.sh
+++ finetune_qm9_pretrained.sh
@@ -3,6 +3,6 @@
 DGL_PYTHON_PATH="/home/tzongzhi/anaconda3/envs/dgl-geognn/bin/python"
 
 $DGL_PYTHON_PATH train_reaction.py \
-    --notes "finetune qm9 -> wb97" \
+    --notes "finetune qm9 (epoch 25) -> wb97" \
     --epochs 100 \
-    --pretrained_encoder_chkpt_path "/home/tzongzhi/mygnn/lightning_logs/version_115/checkpoints/qm9-epoch=76-std_val_loss=0.10.ckpt" \
+    --pretrained_encoder_chkpt_path "/home/tzongzhi/mygnn/lightning_logs/version_125/checkpoints/epoch=25-std_val_loss=1.18e-01.ckpt" \
