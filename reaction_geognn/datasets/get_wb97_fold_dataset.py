684b7ad2bab547760289e293a5ed8c0ccf426dc4
EvitanRelta <shauntanzongzhi@gmail.com>
2023-07-27 09:23:27 +0800

    Minor refactor

--- reaction_geognn/datasets/get_wb97_fold_dataset.py
+++ reaction_geognn/datasets/get_wb97_fold_dataset.py
@@ -1,36 +1,35 @@
-from typing import Literal, overload
+from typing import Literal, TypeAlias, overload
 
 from utils import abs_path
 
 from .Wb97SplitDataset import Wb97SplitDataset
 
+Wb97FoldSplitTuple: TypeAlias = tuple[Wb97SplitDataset, Wb97SplitDataset, Wb97SplitDataset]
+B97FoldSplitTuple: TypeAlias = Wb97FoldSplitTuple
 
 @overload
 def get_wb97_fold_dataset(
     fold_num: Literal[0, 1, 2, 3, 4],
     include_pretrain: Literal[True],
-) -> tuple[
-    tuple[Wb97SplitDataset, Wb97SplitDataset, Wb97SplitDataset],
-    tuple[Wb97SplitDataset, Wb97SplitDataset, Wb97SplitDataset],
-]: ...
+) -> tuple[Wb97FoldSplitTuple, B97FoldSplitTuple]: ...
 @overload
 def get_wb97_fold_dataset(
     fold_num: Literal[0, 1, 2, 3, 4],
     include_pretrain: Literal[False] = False,
-) -> tuple[Wb97SplitDataset, Wb97SplitDataset, Wb97SplitDataset]: ...
+) -> Wb97FoldSplitTuple: ...
 def get_wb97_fold_dataset(
     fold_num: Literal[0, 1, 2, 3, 4],
     include_pretrain: bool = False,
 ):
-    split_dir = './wb97xd3/splits'
+    SPLIT_DIR = './wb97xd3/splits'
 
-    train_path = abs_path(f'{split_dir}/fold_{fold_num}/aam_train.csv', __file__)
-    test_path = abs_path(f'{split_dir}/fold_{fold_num}/aam_test.csv', __file__)
-    val_path = abs_path(f'{split_dir}/fold_{fold_num}/aam_val.csv', __file__)
+    train_path = abs_path(f'{SPLIT_DIR}/fold_{fold_num}/aam_train.csv', __file__)
+    test_path = abs_path(f'{SPLIT_DIR}/fold_{fold_num}/aam_test.csv', __file__)
+    val_path = abs_path(f'{SPLIT_DIR}/fold_{fold_num}/aam_val.csv', __file__)
 
-    pretrain_train_path = abs_path(f'{split_dir}/fold_{fold_num}/aam_pretrain_train.csv', __file__)
-    pretrain_test_path = abs_path(f'{split_dir}/fold_{fold_num}/aam_pretrain_test.csv', __file__)
-    pretrain_val_path = abs_path(f'{split_dir}/fold_{fold_num}/aam_pretrain_val.csv', __file__)
+    pretrain_train_path = abs_path(f'{SPLIT_DIR}/fold_{fold_num}/aam_pretrain_train.csv', __file__)
+    pretrain_test_path = abs_path(f'{SPLIT_DIR}/fold_{fold_num}/aam_pretrain_test.csv', __file__)
+    pretrain_val_path = abs_path(f'{SPLIT_DIR}/fold_{fold_num}/aam_pretrain_val.csv', __file__)
 
     datasets = (
         Wb97SplitDataset(train_path),
