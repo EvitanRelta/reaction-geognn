e3afbcc566fdb281349347cf0886128444f27f2f
EvitanRelta <shauntanzongzhi@gmail.com>
2023-07-27 16:59:51 +0800

    Remove exports from 'geognn.features' package to avoid bloat

--- geognn/features/__init__.py
+++ geognn/features/__init__.py
@@ -1,9 +1,4 @@
-from .atom_features import LABEL_ENCODED_ATOM_FEATURES, atom_pos, atomic_num, \
-    chiral_tag, degree, formal_charge, hybridization, is_aromatic, \
-    total_numHs
+from .atom_features import LABEL_ENCODED_ATOM_FEATURES
 from .base_feature_classes import Feature, FloatFeature, LabelEncodedFeature
-from .bond_angle_features import FLOAT_BOND_ANGLE_FEATURES, bond_angle
-from .bond_features import FLOAT_BOND_FEATURES, LABEL_ENCODED_BOND_FEATURES, \
-    bond_dir, bond_length, bond_type, is_in_ring
-from .rdkit_type_aliases import Atom, Bond, Conformer, Mol, RDKitEnum, \
-    RDKitEnumValue
+from .bond_angle_features import FLOAT_BOND_ANGLE_FEATURES
+from .bond_features import FLOAT_BOND_FEATURES, LABEL_ENCODED_BOND_FEATURES
