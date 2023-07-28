da5d94a07826fea6a71bcf8efd79a30686897ee3
EvitanRelta <shauntanzongzhi@gmail.com>
2023-07-28 14:13:39 +0800

    Rename 'DownstreamModel.py' -> 'model.py'

--- geognn/__init__.py
+++ geognn/__init__.py
@@ -1,3 +1,3 @@
-from .DownstreamModel import DownstreamModel
 from .encoder_model import GeoGNNLayer, GeoGNNModel, InnerGNN
+from .model import DownstreamModel
 from .preprocessing import smiles_to_graphs
