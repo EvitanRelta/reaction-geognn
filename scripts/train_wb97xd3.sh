ae23d5a63b1a5f3f3bdcf226f68a8d30a5f981ca
EvitanRelta <shauntanzongzhi@gmail.com>
2023-07-28 09:43:20 +0800

    Fix the shell script paths

--- scripts/train_wb97xd3.sh
+++ scripts/train_wb97xd3.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 DGL_PYTHON_PATH="/home/tzongzhi/anaconda3/envs/dgl-geognn/bin/python"
 
-$DGL_PYTHON_PATH train_reaction.py \
+$DGL_PYTHON_PATH "$SCRIPT_DIR/../train_reaction.py" \
     --notes "no pretraining" \
